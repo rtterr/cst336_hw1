edb8bf50a9dafe1c147f9282e20648bfc32da5ec
EC2 Default User <ec2-user@ip-172-31-74-48.ec2.internal>
2020-11-04 01:48:31 +0000

    Initial update

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+
+<!-- This is the head -->
+<!-- All styles and javascript go inside the head -->
+    <head>
+        <meta charset="utf-8" />
+        <title>Information Security</title>
+        <link href="css/styles.css" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css2?family=Turret+Road:wght@700&display=swap" rel="stylesheet"> 
+    </head>
+<!-- closing head -->
+
+    <!-- This is the body -->
+    <!-- This is where we place the content of our website -->
+    <body>
+        <header>
+            <h1>Information Security</h1>
+        </header>
+        <nav>
+            <a href="index.php" id="currentpage">About</a>
+            <a href="confidentiality.html">Confidentiality</a>
+            <a href="integrity.html">Integrity</a>
+            <a href="availability.html">Availability</a>
+        </nav>
+        <br /><br />
+        <main>
+            <figure>
+                <img src="img/about.jpg" id="mainimg" alt="Information Security" />
+                <figcaption>Image Source: <a href="https://www.123rf.com/photo_64366454_digital-security-concept.html" target="_blank">123RF</a></figcaption>
+            </figure>
+            <div id="content">
+                <h2>About</h2>
+                In today's technology-reliant world, <b>information security</b> has become more important than ever
+                in order to protect the computer systems that house and transport our precious data.
+                <br /><br />
+                There are three primary concepts that summarize how data can be made more (or less) secure: <b>confidentiality</b>, <b>integrity</b>, and <b>availability</b>.
+                Systems that fulfill all three concepts can be said to be comprehensively secure.
+                <hr />
+                <div id="citations">
+                    Sources:
+                    <ul>
+                        <li><a href="https://www.iso.org/obp/ui/#iso:std:iso-iec:27000:ed-5:v1:en" target="_blank">ISO/IEC 27000:2018(en): Information technology</a></li>
+                        <li><a href="https://www.certmike.com/confidentiality-integrity-and-availability-the-cia-triad/" target="_blank">Confidentiality, Integrity and Availability – The CIA Triad</a></li>
+                    </ul>
+                </div>
+    
+            </div>
+        </main>
+        
+        <!-- This is the footer -->
+        <!-- The footer goes inside the body but not always -->
+        <footer>
+            <hr>
+            CST336 Internet Programming. 2020&copy;Terrado <br />
+            <strong>Disclaimer:</strong> The information in this webpage is fictitious. </br >
+            It is used for academic purposes only.
+            <br /><br /><img src="img/csumb_logo.png" />
+        </footer>
+        <!-- closing footer -->
+        
+    </body>
+    <!-- closing body -->
+
+</html>
